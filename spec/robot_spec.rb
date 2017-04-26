a2761834c55c13e8b25782114e1576125aaa4794
iancwe <crubbishhute@gmail.com>
2017-04-26 10:32:01 +0800

    damn rubocop fixed some stuff

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -13,8 +13,8 @@ require_relative'../robot'
 
 describe Robot do
   describe 'Initialization' do
-    it'should create new robot name'do
-      r1=Robot.new
+    it'should create new robot name' do
+      r1 = Robot.new
       expect(r1.name.length).to exist
     end
   end
